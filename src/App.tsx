ea01b13294270a95cb1dc93d2995bb83d3b79b45
David M. Chen <chen7david@me.com>
2023-07-02 13:15:06 -0300

    added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
-
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { ContactUs } from './pages/ContactUs'
+import { About } from './pages/About'
+import { Link } from 'react-router-dom';
 export const App = () => {
-  const [count, setCount] = useState(1)
+
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Adds</button>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/contact-us'>Contact Us</Link>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact-us' element={<ContactUs/>} />
+      </Routes>
     </>
   )
 }
