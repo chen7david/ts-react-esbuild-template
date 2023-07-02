c5f8f8de1f16707bb6bcbec25af47cccd6b91eb2
David M. Chen <chen7david@me.com>
2023-07-02 12:16:54 -0300

    added prettier config

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom/client'
 import { App } from './App'
 
 ReactDom.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
